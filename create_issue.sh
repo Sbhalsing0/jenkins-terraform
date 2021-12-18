0a33fee92c36162a3246a222ecfaa77bcdb34182
Sanket Bhalsing <42617962+Sbhalsing0@users.noreply.github.com>
2021-12-18 14:45:08 +0530

    shell script

---
+++ create_issue.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#gh auth login --with-token < secret.txt
+cd /home/ubuntu/Documents/
+git clone git@github.com:Sbhalsing0/jenkins-terraform.git
+ls
+cd jenkins-terraform
+commit=$(git log |  awk '{print $2}' | head -n1)
+echo $commit
+gh issue create --title "Build Failure" --body "Root cause : $commit" --label "Incident"
