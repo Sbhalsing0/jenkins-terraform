ede50fba1f7073fd1089127aa62deb861aae0295
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-12-20 17:21:44 +0530

    Update dice.sh

--- dice.sh
+++ dice.sh
@@ -40,7 +40,27 @@ then
     echo "Pipeline will failed"
     docker --version
     exit 1
-
+elif [ "$throw" == '8' ] 
+then
+    echo "pwd"
+    sh create_issue.sh
+    echo "Pipeline will failed"
+    docker --version
+    exit 1
+elif [ "$throw" == '5' ] 
+then
+    echo "pwd"
+    sh create_issue.sh
+    echo "Pipeline will failed"
+    docker --version
+    exit 1
+elif [ "$throw" == '7' ] 
+then
+    echo "pwd"
+    sh create_issue.sh
+    echo "Pipeline will failed"
+    docker --version
+    exit 1
 else
     sh close_issue.sh
     echo "PASS"
